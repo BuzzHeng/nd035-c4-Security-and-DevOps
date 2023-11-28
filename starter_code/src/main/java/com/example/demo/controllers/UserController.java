3984f4180b6f7c3b7fac23312635946f88ee2cf8
BuzzHeng <leeyuanheng16@gmail.com>
2023-11-28 13:11:49 +0800

    Changes
    Changes

--- starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -1,10 +1,13 @@
 package com.example.demo.controllers;
 
+import java.security.SecureRandom;
+import java.util.Base64;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,15 +21,21 @@ import com.example.demo.model.persistence.repositories.CartRepository;
 import com.example.demo.model.persistence.repositories.UserRepository;
 import com.example.demo.model.requests.CreateUserRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-	
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
 	@Autowired
 	private UserRepository userRepository;
 	
 	@Autowired
 	private CartRepository cartRepository;
+
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@GetMapping("/id/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
@@ -46,6 +55,23 @@ public class UserController {
 		Cart cart = new Cart();
 		cartRepository.save(cart);
 		user.setCart(cart);
+		if(createUserRequest.getPassword().length() < 7 ||
+				!createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())){
+				log.error("Error with user password. Cannot create User {}", createUserRequest.getUsername());
+				return ResponseEntity.badRequest().build();
+		}
+
+		// generate a random salt
+		SecureRandom random = new SecureRandom();
+		byte[] salt = new byte[16];
+		random.nextBytes(salt);
+		String encodedSalt = Base64.getEncoder().encodeToString(salt);
+
+		// append salt to password and hash it
+		String saltedPassword = createUserRequest.getPassword() + encodedSalt;
+		user.setPassword(bCryptPasswordEncoder.encode(saltedPassword));
+		user.setSalt(encodedSalt);
+
 		userRepository.save(user);
 		return ResponseEntity.ok(user);
 	}
